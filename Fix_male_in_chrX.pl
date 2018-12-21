bc31ae61efd8b4cc3810468c331eb1faabde880f
gedoardo83 <edoardo.giacopuzzi@unibs.it>
2018-12-21 12:07:10 +0100

    Fix male het calls in non-PAR regions

---
+++ Fix_male_in_chrX.pl
@@ -0,0 +1,29 @@
+#Remove het call in male samples in NON-PAR regions
+#ARGV[0] = vcf of chrX for all samples
+#ARGV[1] = list of samples with sex code (1 male / 2 female) in the same order as VCF samples
+
+open(IN, $ARGV[1]);
+$n=9;
+while ($row=<IN>) {
+  $n++;
+  chomp($row);
+  @line = split(" ", $row);
+  if ($line[1] == 1) {push(@males, $n)}
+}
+close(IN);
+
+open(IN, $ARGV[0]);
+while ($row=<IN>) {
+  chomp($row);
+  if ($row =~ /^#/) {
+    print "$row\n";
+    next;
+  }
+
+  @line = split("\t", $row);
+  foreach $id(@males) {
+    if ($line[1] >= 2699520 && $line[1] <= 154931043 && $line[$id] eq "0/1") {$line[$id] = "./."}
+  }
+  print "$row\n";
+}
+close(IN);
